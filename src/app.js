372fe94333c3aba09ea1e5a98dc92a9d164d33af
geraldo gomes <contato.gneto@gmail.com>
2023-07-09 02:26:13 -0300

    add new feature: post request for user sign-in.

--- src/app.js
+++ src/app.js
@@ -53,7 +53,35 @@ app.post('/cadastro', async (req, res) => {
    }
 })
 
+app.post('/', async (req, res) => {
+   const { email, password } = req.body
 
+   const schemaSignInUser = Joi.object({
+      email: Joi.string().email({ maxDomainSegments: 2 }).required(),
+      password: Joi.string().required()
+   })
+
+   const validate = schemaSignInUser.validate(req.body, { abortEarly: false })
+
+   if (validate.error) {
+      const errors = validate.error.details.map((detail) => detail.message)
+      return res.status(422).send(errors)
+   }
+
+   try {
+      const user = await db.collection('users').findOne({ email })
+
+      if (!user) return res.status(404).send('Usuário não cadastrado!')
+      if (!bcrypt.compareSync(password, user.password))
+         return res.status(401).send('Senha incorreta!')
+
+      const token = uuid()
+      await db.collection('sessions').insertOne({ token, userID: user._id })
+      res.status(200).send(token)
+   } catch (error) {
+      res.status(500).send(error.message)
+   }
+})
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
